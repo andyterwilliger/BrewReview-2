f1b6294b777719f4adee4d820c2c8de535095f13
Andy Terwilliger <andy@terwilliger1@gmail.com>
2022-02-20 20:07:18 -0600

    fully transitioned from old brewreview, Beer model connected to form

--- main_app/models.py
+++ main_app/models.py
@@ -7,8 +7,16 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 class Beer(models.Model):
-
+    brewery= models.CharField(max_length=50)
+    name= models.CharField(max_length=50)
+    profile=models.CharField(max_length=50)
 
 
 
     user = models.ForeignKey(User, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('feed')
